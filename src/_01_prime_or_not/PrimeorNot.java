f01da727d2a8bd41b50fba36f303e2885f2a0c7b
League Student <league@iMac-14.attlocal.net>
2019-10-03 18:28:43 -0700

    :sunglasses:

--- src/_01_prime_or_not/PrimeorNot.java
+++ src/_01_prime_or_not/PrimeorNot.java
@@ -1,9 +1,23 @@
 package _01_prime_or_not;
 
+import javax.swing.JOptionPane;
+
 public class PrimeorNot {
-public static void main(String[] args) {
-	for (;true;) {
-		
+	public static void main(String[] args) {
+		String s = JOptionPane.showInputDialog(null, "What number do you want to test if it's prime or not?");
+		int i = Integer.parseInt(s);
+		for (int j = 1; j < i; j++) {
+			if (0 == i % j && j!=1 && j!=i) {
+				
+					JOptionPane.showMessageDialog(null, "Your number is not prime.");
+					break;
+
+				} else {
+					JOptionPane.showMessageDialog(null, "Your number is a prime number.");
+					break;
+				}
+
+			}
+		}
 	}
-}
-}
+

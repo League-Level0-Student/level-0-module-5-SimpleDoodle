7b0d5ae470e854759c6d15087b42f2d66e41969b
League Student <league@iMac-24.attlocal.net>
2019-10-17 18:31:04 -0700

    :man_with_gua_pi_mao:

--- src/_03_goofy_names/GoofyNames.java
+++ src/_03_goofy_names/GoofyNames.java
@@ -1,6 +1,6 @@
 package _03_goofy_names;
 /*
- *    Copyright (c) The League of Amazing Programmers 2013-2017
+ *    Copyright (c) The League of Amazing Programmers 2013-2019
  *    Level 0
  */
 
@@ -11,26 +11,32 @@ public class GoofyNames {
 		String goofyName = "";
 
 		// 1. Ask the user to enter their name
-
+		String name = JOptionPane.showInputDialog(null, "What is your name?");
 		// 2. Print upper case name to the console using .toUpperCase()
-		//    Run your program to see that this works.
-
+		// Run your program to see that this works.
+		// System.out.println(name.toUpperCase());
+		int i = name.length();
 		// 3. Loop through each character of the name (steps 4 - 6).
-		//    HINT: Use .length() to determine the number of characters in the String.
+		// HINT: Use .length() to determine the number of characters in the String.
+		for (int j = 0; j < i; j++) {
+			String l = name.substring(j, j + 1);
+			if (j % 2 == 0) {
+				goofyName += (l.toUpperCase());
+			} else {
+				goofyName += (l.toLowerCase());
+			}
+		}
+		JOptionPane.showMessageDialog(null, goofyName);
+		// 4. Create a String variable to store the next character of the name
+		// using .substring(start, end)
+		// HINT: replace 'start' and 'end' to get String with character at i
 
-			// 4. Create a String variable to store the next character of the name
-			//    using .substring(start, end)
-			//    HINT: replace 'start' and 'end' to get String with character at i 
-			
-			// 5. Use MODULO to set this variable to upper case for EVEN characters
-			//    and lower case for ODD characters.
+		// 5. Use MODULO to set this variable to upper case for EVEN characters
+		// and lower case for ODD characters.
 
-			
-			// 6. ADD this String  (containing 1 char) to the goofyName String
+		// 6. ADD this String (containing 1 char) to the goofyName String
 
-		
 		// 7. Use pop-up to show user their Goofy name
 
 	}
 }
-

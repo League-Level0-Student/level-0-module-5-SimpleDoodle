b5046697a9a7430b455bba11ca427d951d320ba9
League Student <league@iMac-11.attlocal.net>
2019-11-14 18:29:20 -0800

    :shaved_ice:

--- src/_06_for_loop_gauntlet/ForLoopGauntlet.java
+++ src/_06_for_loop_gauntlet/ForLoopGauntlet.java
@@ -48,14 +48,21 @@ public class ForLoopGauntlet {
 //	b+=3;
 //	}
 //}
-		int a = 2;
-		int b = 3;
-		for (int i = 1; i < 11; i++) {
-			for (int j = 0; j < 1; j++) {
-				System.out.println(i + " " + a + " " + b);
-				a += 3;
-				b += 3;
+//		int a = 0;
+//		for (int i = 1; i < 11; i++) {
+//			for (int j = 1; j < 11; j++) {
+//				System.out.print(" " + (j+a));
+//			}
+//			a+=10;
+//			System.out.println();
+//		}
+//	}
+//}
+		for (int i = 1; i < 7; i++) {
+			for (int j = 0; j < i; j++) {
+				System.out.print("* ");
 			}
+			System.out.println();
 		}
 	}
 }
